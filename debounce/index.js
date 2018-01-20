9042e839dcb41453f700c9fee998e4f882488eab
Johnmark Beaty <johnmarkbeaty@gmail.com>
2018-01-20 04:00:07 +0000

    feat(debounce): add debounce function

---
+++ debounce/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = (fn, wait = 100) => {
+  let timeout;
+  return function(...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => fn.apply(this, args), wait);
+  };
+};
